0c2683e2792d5c017ea957761b248dcedd4b4b89
thisiszammy <thisiszammy@gmail.com>
2024-09-28 10:00:19 +0800

    [zammy][Test] Changed local repo user

--- ASI.Basecode.Data/Models/User.cs
+++ ASI.Basecode.Data/Models/User.cs
@@ -5,6 +5,7 @@ namespace ASI.Basecode.Data.Models
 {
     public partial class User
     {
+        // test change
         public int Id { get; set; }
         public string UserId { get; set; }
         public string Name { get; set; }
